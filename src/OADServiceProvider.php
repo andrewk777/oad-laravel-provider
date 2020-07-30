7e9fb765721536a926ad6f988896c2e09d954f0f
Andrew <andrew@oadsoft.com>
2020-07-30 13:16:00 -0400

    updates

--- src/OADServiceProvider.php
+++ src/OADServiceProvider.php
@@ -12,24 +12,29 @@ class OADServiceProvider extends ServiceProvider {
         $this->loadRoutesFrom(__DIR__.'/routes.php');
 
         //loading views
-        $this->loadViewsFrom(__DIR__ . '/views' , 'oadspa');
+        $this->loadViewsFrom(__DIR__ . '/views' , 'oadspa');        
 
-        //deleting default migrations
-        $migration_files = array_diff(scandir(database_path('migrations')), array('..', '.'));
-        foreach ($migration_files as $file_name) {
-            if (strstr($file_name,'users_table')) {
-                unlink(database_path('migrations/' . $file_name));
+        //this should run only once
+        if (file_exists(app_path('User.php'))) {
+
+            // deleting default User Model File
+            unlink(app_path('User.php'));
+
+            //deleting default env files
+            if (file_exists(base_path('.env.example'))) unlink(base_path('.env.example'));
+
+            //deleting default migrations
+            $migration_files = array_diff(scandir(database_path('migrations')), array('..', '.'));
+            foreach ($migration_files as $file_name) {
+                if (strstr($file_name,'users_table')) {
+                    unlink(database_path('migrations/' . $file_name));
+                }
             }
+
+            // deleting default User Model File
+            if (file_exists(config_path('auth.php'))) unlink(config_path('auth.php'));
+
         }
-
-        //deleting default env files
-        if (file_exists(base_path('.env.example'))) unlink(base_path('.env.example'));
-
-        // deleting default User Model File
-        if (file_exists(app_path('User.php'))) unlink(app_path('User.php'));
-
-        // deleting default User Model File
-        if (file_exists(config_path('auth.php'))) unlink(config_path('auth.php'));
 
         $this->publishes([
             __DIR__.'/Commands' => app_path('Console/Commands'),
