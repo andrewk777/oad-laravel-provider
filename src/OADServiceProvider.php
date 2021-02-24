18a4874030e8a4f0afb73efec71b8f5e6675d2c1
Andrew <andrew@oadsoft.com>
2021-02-24 15:33:10 -0500

    provider updates

--- src/OADServiceProvider.php
+++ src/OADServiceProvider.php
@@ -9,12 +9,13 @@ class OADServiceProvider extends ServiceProvider {
     public function boot() {
 
         //loading web and api routes
-        $this->loadRoutesFrom(__DIR__.'/routes.php');      
+        $this->loadRoutesFrom(__DIR__.'/routes.php');     
+        
+        $this->loadViewsFrom(__DIR__.'/../publish/resources/views', 'oadsoft');
 
         $this->publishes([
             __DIR__.'/../publish/Http/Controllers'      => app_path('Http/Controllers'),
-            __DIR__.'/../publish/Console'               => app_path('Console'),
-            __DIR__.'/../publish/resources'             => app_path('resources')
+            __DIR__.'/../publish/Console'               => app_path('Console')
         ]);
 
     }
